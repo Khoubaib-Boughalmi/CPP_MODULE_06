ac00572545246b02d9a7c519d71b2fb41105386d
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-12 17:47:00 +0100

    fixed signed numbers

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,18 @@
 #include "ScalarConverter.h"
 
+class ErrorExeption: public std::exception {
+    public:
+        const char *what() const throw();
+};
+
+const char *ErrorExeption::what() const throw() {
+    return "ErrorExeption";
+}
+
 int isDigits(std::string input) {
     int i = 0;
+    if(input[i] == '+' || input[i] == '-' )
+        i++;
     while (input[i])
     {
         if(!std::isdigit(input[i]))
@@ -28,6 +39,8 @@ int detectDecimal(std::string input)
 int detectFloat(std::string input)
 {
     int i = 0;
+    if(input[i] == '+' || input[i] == '-' )
+        i++;
     while (input[i] && std::isdigit(input[i]))
         i++;
     if(input[i] == '.')
@@ -44,6 +57,8 @@ int detectFloat(std::string input)
 int detectDouble(std::string input)
 {
     int i = 0;
+    if(input[i] == '+' || input[i] == '-' )
+        i++;
     while (input[i] && std::isdigit(input[i]))
         i++;
     if(input[i] == '.')
@@ -68,23 +83,22 @@ int detectScalarType(std::string input)
     return (0);
 }
 
-char convertChar(std::string input) {
-    return input[0];
+void convertChar(std::string input) {
+    std::cout << "Char formal: " << GREEN << "'" << input << "'" << std::endl;
 }
-char convertDecimal(std::string input) {
+void convertDecimal(std::string input) {
     char *ptr;
 
     errno = 0;
     long result;
     result = std::strtol(input.c_str(), &ptr, 10);
-    if(!*ptr)
-        return (result);
     if(errno == ERANGE)
-        throw;
-    return (result);
+        throw ErrorExeption();
+    std::cout << "Decimal formal: " << GREEN << result << std::endl;
 }
-char convertFloat(std::string input) {}
-char convertDouble(std::string input) {}
+
+void convertFloat(std::string input) {(void)input;}
+void convertDouble(std::string input) {(void)input;}
 
 int main(int argc, char *argv[])
 {
@@ -95,24 +109,30 @@ int main(int argc, char *argv[])
         return (1);
     }
     int scalarType = detectScalarType(static_cast<std::string>(argv[1]));
-    std::cout << scalarType << std::endl;
-    switch (scalarType)
+    try
     {
-        case CHAR_TYPE:
-            convertChar(argv[1]);
-            break;
-        case DECIMAL_TYPE:
-            convertDecimal(argv[1]);
-            break;
-        case FLOAT_TYPE:
-            convertFloat(argv[1]);
-            break;
-        case DOUBLE_TYPE:
-            convertDouble(argv[1]);
-            break;
-        case NO_TYPE:
-        std::cout << "impossible" << std::endl;
+        switch (scalarType)
+        {
+            case CHAR_TYPE:
+                convertChar(argv[1]);
+                break;
+            case DECIMAL_TYPE:
+                convertDecimal(argv[1]);
+                break;
+            case FLOAT_TYPE:
+                convertFloat(argv[1]);
+                break;
+            case DOUBLE_TYPE:
+                convertDouble(argv[1]);
+                break;
+            case NO_TYPE:
+            std::cout << RED << "impossible" << std::endl;
+        }
     }
-   
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+  
     return (0);
 }
