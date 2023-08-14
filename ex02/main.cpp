284a11935fb8228f32fe41d4502528ad86861197
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-14 16:51:05 +0100

    fixed and finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,37 +5,81 @@
 
 Base * generate(void) {
     std::srand(static_cast<unsigned int>(std::time(NULL)));
-    int randVal = std::rand() % 3;
-    if(randVal == 0)
+    int randVal = (std::rand() % 10);
+    if(randVal >= 0 && randVal <= 3 )
         return new A();
-    if(randVal == 1)
+    if(randVal >= 4 && randVal <= 6 )
         return new B();
-    if(randVal == 2)
+    if(randVal >= 7 && randVal <= 9 )
         return new C();
     return (0);
 }
 
-int main() {
-    Base *p = generate();
+void identify(Base* p)
+{
     A *classA = dynamic_cast<A*>(p);
     if(classA)
     {
         std::cout << "A" << std::endl;
-        delete p;
-        return (0);
+        return ;
     }
     B *classB = dynamic_cast<B*>(p);
     if(classB)
     {
         std::cout << "B" << std::endl;
-        delete p;
-        return (0);
+        return ;
     }
     C *classC = dynamic_cast<C*>(p);
     if(classC)
     {
         std::cout << "C" << std::endl;
-        delete p;
-        return (0);
+        return ;
     }
+}
+
+void identify(Base& p)
+{
+    bool identified = false;
+    try
+    {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" <<std::endl;
+        identified = true;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    if(!identified)
+    {
+        try
+        {
+            (void)dynamic_cast<B&>(p);
+            std::cout << "B" <<std::endl;
+            identified = true;
+        }
+        catch(const std::exception& e)
+        {
+            (void)e;
+        }
+    }
+    if(!identified)
+    {
+        try
+        {
+            (void)dynamic_cast<C&>(p);
+            std::cout << "C" <<std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            (void)e;
+        }
+    }
+}
+
+int main() {
+    Base *p = generate();
+    identify(p);
+    identify(*p);
+    delete p;
 }
