f9e6d7d9859c4afa2cb6d6cfedbd65ab8c57d618
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-08-14 13:10:56 +0100

    fixed memory leak in ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -21,18 +21,21 @@ int main() {
     if(classA)
     {
         std::cout << "A" << std::endl;
+        delete p;
         return (0);
     }
     B *classB = dynamic_cast<B*>(p);
     if(classB)
     {
         std::cout << "B" << std::endl;
+        delete p;
         return (0);
     }
     C *classC = dynamic_cast<C*>(p);
     if(classC)
     {
         std::cout << "C" << std::endl;
+        delete p;
         return (0);
     }
 }
